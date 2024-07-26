e6a9dbc14967ee0cc57166ae8f4fb7d5ba4dba74
Romerio Ribeiro <romerio111@hotmail.com>
2024-07-26 18:03:53 -0300

    feat(trip-detaisl): create 'create-important-link'

---
+++ src/pages/trip-details/create-important-link-modal.tsx
@@ -0,0 +1,72 @@
+import { FormEvent } from "react";
+import { Link, Tag, X } from "lucide-react";
+
+import { Button } from "@/components/button";
+import { useLinks } from "@/data/contexts/links";
+
+interface CreateImportantLinkModalProps {
+  closeCreateImportantLinkModal: () => void;
+}
+
+export function CreateImportantLinkModal({
+  closeCreateImportantLinkModal,
+}: CreateImportantLinkModalProps) {
+  const { createLinks } = useLinks();
+
+  async function createLink(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    const title = data.get("title")?.toString() || "";
+    const url = data.get("url")?.toString() || "";
+
+    await createLinks({ title, url });
+
+    closeCreateImportantLinkModal();
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <div className="space-y-2">
+          <div className="flex items-center justify-between">
+            <h2 className="font-lg font-semibold">Cadastrar Link</h2>
+            <button>
+              <X
+                className="size-5 text-zinc-400"
+                onClick={closeCreateImportantLinkModal}
+              />
+            </button>
+          </div>
+
+          <p className="text-sm text-zinc-400">
+            Todos convidados podem visualizar os links.
+          </p>
+        </div>
+
+        <form onSubmit={createLink} className="space-y-3">
+          <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <Tag className="text-zinc-400 size-5" />
+            <input
+              name="title"
+              placeholder="Descreva o link (Ex.: Hotel hospedagem)"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+
+          <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <Link className="text-zinc-400 size-5" />
+            <input
+              name="url"
+              placeholder="Qual o link? (Ex.: https://www.exemplo.com)"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
+          </div>
+
+          <Button size="full">Salvar Link</Button>
+        </form>
+      </div>
+    </div>
+  );
+}
